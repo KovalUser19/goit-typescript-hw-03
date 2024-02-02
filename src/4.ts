98555e65a9661057050b4edf43fbdafa17ea1ecb
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-02-02 20:30:07 +0200

    add

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+ abstract class House{
+ protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void{
+    if (this.door) {
+      this.tenants.push(person);
+    } else {
+      console.log('The door is closed')
+    }
+  }
+}
+
+class Person{
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key{
+    return this.key;
+  }
+}
+
+class Key{
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number{
+    return this.signature;
+  }
+}
+
+class MyHouse extends House{
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+   openDoor(key: Key): void {
+     if (key.getSignature() === this.key.getSignature()) {
+       this.door = true;
+     } else {
+       this.door = false;
+     }
+ }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
